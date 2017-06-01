44d7e6a73d6259a6bcb013a7796fee41103f26d5
BeijiYang <langyuyang@outlook.com>
2017-06-01 17:00:37 +0800

    add mongoose

--- server/config.default.js
+++ server/config.default.js
@@ -1,3 +1,4 @@
 module.exports = {
-  "port": XXX,
+  "port": XXXX,
+  "uri": XXXX,
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const config = require("./config");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect(config.uri);
+
+db = mongoose.connection;
+db.on("error", function(err) {
+  console.log('mongoose connection failed!', err);
+});
+db.once("open", function() {
+  console.log("mongoose success!URI: "+config.uri);
+})
 
 app.get("/api", function(req, res) {
   res.send("viva la vida !")
 })
 
 app.listen(config.port ,function() {
-  console.log("running on port: " + config.port);
+  console.log("express running on port: " + config.port);
 });
